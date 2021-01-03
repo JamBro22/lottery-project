61c31c872742daadd6e48b1e5c2851b28add1968
Jamie Brown <jmb.black3nd@gmail.com>
2021-01-03 11:46:09 +0200

    refactor(general_functions): ensured that a list is passed in as a param

--- general_functions.py
+++ general_functions.py
@@ -1,7 +1,11 @@
-# file for common functions used across the app
+# file for common functions used across the programme
 
 # check a list for duplicates
 def check_list_for_duplicates(item, listed_items):
+    # make sure a list is present
+    if type(listed_items) is not list:
+        return
+
     # return if list is empty
     if len(listed_items) == 0:
         return
@@ -11,4 +15,3 @@ def check_list_for_duplicates(item, listed_items):
         return True
 
     return False
-
