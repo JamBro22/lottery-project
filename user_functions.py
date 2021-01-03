cc8d2d51fa09958ff9aebf487387f38d1f3ad1a4
Jamie Brown <jmb.black3nd@gmail.com>
2021-01-03 11:39:53 +0200

    feature(user_functions): allowed user to add numbers to list without duplicates in get_user_numbers function

--- user_functions.py
+++ user_functions.py
@@ -1,3 +1,7 @@
+# import user-defined modules
+from general_functions import check_list_for_duplicates
+
+
 # get user age through input
 def get_user_age():
     try:
@@ -20,17 +24,26 @@ def is_older_than_18(age):
 
 # return a list of non-duplicated user chosen numbers
 def get_user_numbers():
-    # user_numbers = []
+    user_numbers = []
 
     i = 1
     while i <= 6:
         number = int(input("Please enter a number from 1 to 49: "))
+
+        is_duplicated = check_list_for_duplicates(number, user_numbers)
+
+        if is_duplicated:
+            print("Number already chosen")
+            continue
 
         if 1 > number or number > 49:
             print("Number must be from 1 to 49")
             continue
 
         i += 1
+        user_numbers.append(number)
+
+    return user_numbers.sort()
 
 
-get_user_numbers()
+print(get_user_numbers())
